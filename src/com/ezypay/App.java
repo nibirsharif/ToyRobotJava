557ad84f4bd2dd69d62cb4cb064f3168aadc10e0
Nibir <nibirsharif@outlook.com>
2018-01-28 22:45:18 +0800

    :tada: initial commit

---
+++ src/com/ezypay/App.java
@@ -0,0 +1,9 @@
+package com.ezypay;
+
+public class App {
+
+    public static void main(String[] args) {
+	    // write your code here
+        System.out.println("Toy Robot Simulator.");
+    }
+}

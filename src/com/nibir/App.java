701df492852521e591d7f8502cd825b1cc276d0f
Nibir <nibirsharif@outlook.com>
2018-01-28 23:05:25 +0800

    :sparkles: added Board class

--- src/com/nibir/App.java
+++ src/com/nibir/App.java
@@ -6,6 +6,7 @@ public class App {
 
     private static int x = 0;
     private static int y = 0;
+    private static Board board;
     private static String place;
     private static String command;
     private static Direction direction;
@@ -20,11 +21,24 @@ public class App {
         y = Integer.parseInt(input[1]);
         direction = Direction.valueOf(input[2].toString().toUpperCase());
 
+        board = new Board(4,4);
+
         while (sc.hasNextLine()){
             command  = sc.nextLine();
             instruction = Instruction.valueOf(command.toUpperCase());
 
             if (instruction.equals(Instruction.REPORT)) break;
+
+            switch (instruction) {
+                case MOVE:
+                    break;
+                case LEFT:
+                    break;
+                case RIGHT:
+                    break;
+                default:
+                    break;
+            }
 
         }
         System.out.println(x + "," + y + "," + direction);
---
+++ src/com/nibir/Board.java
@@ -0,0 +1,13 @@
+package com.nibir;
+
+public class Board {
+    private int rows;
+    private int columns;
+
+    public Board() { }
+
+    public Board(int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
+    }
+}
